935207a8825b8715d083a1b816cf18dbeb89a659
Joao MF Alves <jmfernandesalves@gmail.com>
2021-05-19 17:51:50 +0200

    Update BWA.sh

--- QC-Mapping/BWA.sh
+++ QC-Mapping/BWA.sh
@@ -13,3 +13,6 @@ WORKING_DIR="/mnt/lustre/scratch/home/uvi/be/jfa/"
 SAMPLE=$(sed "${SLURM_ARRAY_TASK_ID}q;d" PATIENTID-Samples)
 
 bwa mem -t 1 -M -R "@RG\tID:${SAMPLE}\tSM:${SAMPLE}\tPL:Illumina\tLB:${SAMPLE}\tPU:HiSeq" ${RESOURCES}/hs37d5.fa ${WORKING_DIR}/${PATIENT_ID}/${SAMPLE}".trimmed2_1.fastq.gz ${WORKING_DIR}/${PATIENT_ID}/${SAMPLE}".trimmed2_2.fastq.gz | samtools view -Sbq 20 -@ 12 - > ${WORKING_DIR}/${PATIENT_ID}/${SAMPLE}.bam
+
+samtools sort -o ${WORKING_DIR}/${PATIENT_ID}/${SAMPLE}.sorted.bam ${WORKING_DIR}/${PATIENT_ID}/${SAMPLE}.bam
+samtools index ${WORKING_DIR}/${PATIENT_ID}/${SAMPLE}.sorted.bam
